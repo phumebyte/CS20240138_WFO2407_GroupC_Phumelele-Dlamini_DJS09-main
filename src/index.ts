1b9a3dcfbb5001677a64ba588472c21bcf635d16
phumebyte <phumelele.dlamini1000@gmail.com>
2024-11-24 14:01:43 +0200

    Transferred all files into this repo

---
+++ src/index.ts
@@ -0,0 +1,163 @@
+import { showReviewTotal, populateUser, showDetails, getTopTwoReviews } from './utils';
+import { Permissions, LoyaltyUser } from './enums';
+import { Review, Property } from './interfaces';
+import MainProperty from './classes';
+import './index.css';
+
+
+const propertyContainer = document.querySelector(".properties");
+const reviewContainer = document.querySelector(".reviews");
+const container = document.querySelector(".container");
+const button = document.querySelector("button");
+const footer = document.querySelector(".footer");
+
+// Reviews array of objects
+const reviews: Review[] = [
+  {
+    name: "Sheila",
+    stars: 5,
+    loyaltyUser: LoyaltyUser.GOLD_USER,
+    date: "01-04-2021",
+  },
+  {
+    name: "Andrzej",
+    stars: 3,
+    loyaltyUser: LoyaltyUser.BRONZE_USER,
+    date: "28-03-2021",
+  },
+  {
+    name: "Omar",
+    stars: 4,
+    loyaltyUser: LoyaltyUser.SILVER_USER,
+    date: "27-03-2021",
+  },
+];
+
+const you = {
+  firstName: "Bobby",
+  lastName: "Brown",
+  permissions: Permissions.ADMIN,
+  isReturning: true,
+  age: 35,
+  stayedAt: ["florida-home", "oman-flat", "tokyo-bungalow"],
+};
+
+// Array of  Properties
+const properties: Property[] = [
+  {
+    image: "./images/colombia-property.jpg",
+    title: "Colombian Shack",
+    price: 45,
+    location: {
+      firstLine: "shack 37",
+      city: "Bogota",
+      code: 45632,
+      country: "Colombia",
+    },
+    contact: [+1123495082908, "marywinkle@gmail.com"],
+    isAvailable: true,
+  },
+  {
+    image: "/images/poland-property.jpg",
+    title: "Polish Cottage",
+    price: 30,
+    location: {
+      firstLine: "no 23",
+      city: "Gdansk",
+      code: 343903,
+      country: "Poland",
+    },
+    contact: [+1123495082908, "garydavis@hotmail.com"],
+    isAvailable: false,
+  },
+  {
+    image: "/images/london-property.jpg",
+    title: "London Flat",
+    price: 25,
+    location: {
+      firstLine: "flat 15",
+      city: "London",
+      code: "SW4 5XW",
+      country: "United Kingdom",
+    },
+    contact: [+1123495082908, "andyluger@aol.com"],
+    isAvailable: true,
+  },
+  {
+    image: "/images/malaysian-hotel.jpg",
+    title: "Malia Hotel",
+    price: 35,
+    location: {
+      firstLine: "Room 4",
+      city: "Malia",
+      code: 45334,
+      country: "Malaysia",
+    },
+    contact: [+60349822083, "lee34@gmail.com"],
+    isAvailable: false,
+  },
+];
+
+// Function calls
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
+
+populateUser(you.isReturning, you.firstName);
+
+// Logic
+
+for (let i = 0; i < properties.length; i++) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.innerHTML = properties[i].title;
+  const image = document.createElement("img");
+  image.setAttribute("src", properties[i].image);
+  card.appendChild(image);
+  showDetails(you.permissions, card, properties[i].price);
+  propertyContainer?.appendChild(card);
+}
+
+// for loop individually displays each card on the UI
+let count = 0;
+
+function addReviews(array: Review[]): void {
+  if (!count) {
+    count++;
+    const topTwo = getTopTwoReviews(array);
+    for (let i = 0; i < topTwo.length; i++) {
+      const card = document.createElement("div");
+      card.classList.add("review-card");
+      card.innerHTML = topTwo[i].stars + " stars from " + topTwo[i].name;
+      reviewContainer?.appendChild(card);
+    }
+    if (container && button) {
+      container.removeChild(button);
+    }
+  }
+}
+
+button?.addEventListener("click", () => addReviews(reviews));
+// Replaced with my location, current time and current temperature
+let currentLocation: [string, string, number] = ["Newcastle", "12.02", 31];
+footer!.innerHTML =  currentLocation[0] +  " " +
+  currentLocation[1] +
+  " " +
+  currentLocation[2] +
+  "°C";
+
+let yourMainProperty = new MainProperty(
+  "/images/italian-property.jpg",
+  "Italian House",
+  [
+    {
+      name: "Olive",
+      stars: 5,
+      loyaltyUser: LoyaltyUser.GOLD_USER,
+      date: "12-04-2021",
+    },
+  ]
+);
+
+const mainImageContainer = document.querySelector(".main-image");
+const image = document.createElement("img");
+image.setAttribute("src", yourMainProperty.src);
+mainImageContainer?.appendChild(image);
